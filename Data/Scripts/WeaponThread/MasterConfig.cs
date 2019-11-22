89cf09590d7a70112ccb6f748df234c454396204
Bryon <30876973+nukeguard@users.noreply.github.com>
2019-11-22 07:12:45 -0500

    revert

--- Data/Scripts/WeaponThread/MasterConfig.cs
+++ Data/Scripts/WeaponThread/MasterConfig.cs
@@ -9,7 +9,7 @@
             // Enable your config files using the follow syntax, don't include the ".cs" extension:
             // ConfigFiles(Your1stConfigFile, Your2ndConfigFile, Your3rdConfigFile);
 
-            ConfigFiles(NtechObelisk1);
+            ConfigFiles(NtechObelisk1, NetchObelisk2);
         }
     }
 }
--- Data/Scripts/WeaponThread/NtechObelisk1.cs
+++ Data/Scripts/WeaponThread/NtechObelisk1.cs
@@ -27,17 +27,10 @@ namespace WeaponThread
             {
                 WeaponId = "Ntech Obelisk", // name of weapon in terminal
                 AmmoMagazineId = "Blank",
-<<<<<<< HEAD
-                Block = AimControl(trackTargets: true, turretAttached: false, turretController: false, primaryTracking: true, rotateRate: 0.05f, elevateRate: 0.05f, minAzimuth: -180, maxAzimuth: 180, minElevation: -70, maxElevation: 90, offset: Vector(x: 0, y: .12, z: 0), fixedOffset: false, inventorySize: 0.34f, debug: false),
-                DeviateShotAngle = 0f,
-                AimingTolerance = 180f, // 0 - 180 firing angle
-                EnergyCost = 0.00010001f, //(((EnergyCost * DefaultDamage) * ShotsPerSecond) * BarrelsPerShot) * ShotsPerBarrel
-=======
                 Block = AimControl(trackTargets: true, turretAttached: true, turretController: true, primaryTracking: true, rotateRate: 0.02f, elevateRate: 0.02f, minAzimuth: -180, maxAzimuth: 30, minElevation: -30, maxElevation: 80, offset: Vector(x: 0, y: .12, z: 0), fixedOffset: false, inventorySize: 0.34f, debug: false),
                 DeviateShotAngle = 0.4f,
                 AimingTolerance = .5f, // 0 - 180 firing angle
                 EnergyCost = 0.00000001f, //(((EnergyCost * DefaultDamage) * ShotsPerSecond) * BarrelsPerShot) * ShotsPerBarrel
->>>>>>> parent of d45dcad... more setup
                 Hybrid = false, //projectile based weapon with energy cost
                 EnergyPriority = 0, //  0 = Lowest shares power with shields, 1 = Medium shares power with thrusters and over powers shields, 2 = Highest Does not share power will use all available power until energy requirements met
                 RotateBarrelAxis = 3, // 0 = off, 1 = xAxis, 2 = yAxis, 3 = zAxis
