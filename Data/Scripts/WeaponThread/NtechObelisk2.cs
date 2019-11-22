98a4212baa8923c51e1227e49ef87568110ce1a2
Bryon <30876973+nukeguard@users.noreply.github.com>
2019-11-21 22:19:17 -0500

    revert

--- Data/Scripts/WeaponThread/NtechObelisk2.cs
+++ Data/Scripts/WeaponThread/NtechObelisk2.cs
@@ -13,31 +13,24 @@ namespace WeaponThread
 {   // Don't edit above this line
     partial class Weapons
     {
-        WeaponDefinition NtechObelisk1 => new WeaponDefinition
+        WeaponDefinition NtechObelisk2 => new WeaponDefinition
         {
             Assignments = new ModelAssignments
             {
                 MountPoints = new[]
                 {
-                    MountPoint(subTypeId: "NtechObelisk", aimPartId:"None", muzzlePartId: "Obelisk", azimuthPartId: "None", elevationPartId: "None"),
+                    MountPoint(subTypeId: "NtechObelisk", aimPartId:"", muzzlePartId: "elevation_02", azimuthPartId: "azimuth02", elevationPartId: "elevation_02"),
                 },
-                Barrels = Names("muzzle_barrel_001")
+                Barrels = Names("muzzle_barrel_02")
             },
             HardPoint = new HardPointDefinition
             {
                 WeaponId = "Ntech Obelisk", // name of weapon in terminal
                 AmmoMagazineId = "Blank",
-<<<<<<< HEAD
-                Block = AimControl(trackTargets: true, turretAttached: false, turretController: false, primaryTracking: true, rotateRate: 0.05f, elevateRate: 0.05f, minAzimuth: -180, maxAzimuth: 180, minElevation: -70, maxElevation: 90, offset: Vector(x: 0, y: .12, z: 0), fixedOffset: false, inventorySize: 0.34f, debug: false),
-                DeviateShotAngle = 0f,
-                AimingTolerance = 180f, // 0 - 180 firing angle
-                EnergyCost = 0.00010001f, //(((EnergyCost * DefaultDamage) * ShotsPerSecond) * BarrelsPerShot) * ShotsPerBarrel
-=======
                 Block = AimControl(trackTargets: true, turretAttached: true, turretController: true, primaryTracking: true, rotateRate: 0.02f, elevateRate: 0.02f, minAzimuth: -180, maxAzimuth: 30, minElevation: -30, maxElevation: 80, offset: Vector(x: 0, y: .12, z: 0), fixedOffset: false, inventorySize: 0.34f, debug: false),
                 DeviateShotAngle = 0.4f,
                 AimingTolerance = .5f, // 0 - 180 firing angle
                 EnergyCost = 0.00000001f, //(((EnergyCost * DefaultDamage) * ShotsPerSecond) * BarrelsPerShot) * ShotsPerBarrel
->>>>>>> parent of d45dcad... more setup
                 Hybrid = false, //projectile based weapon with energy cost
                 EnergyPriority = 0, //  0 = Lowest shares power with shields, 1 = Medium shares power with thrusters and over powers shields, 2 = Highest Does not share power will use all available power until energy requirements met
                 RotateBarrelAxis = 3, // 0 = off, 1 = xAxis, 2 = yAxis, 3 = zAxis
@@ -48,18 +41,18 @@ namespace WeaponThread
 
                 Loading = new AmmoLoading
                 {
-                    RateOfFire = 2600,
+                    RateOfFire = 600,
                     BarrelsPerShot = 1,
                     TrajectilesPerBarrel = 1, // Number of Trajectiles per barrel per fire event.
                     SkipBarrels = 0,
-                    ReloadTime = 1, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    ReloadTime = 360, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     DelayUntilFire = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
-                    HeatPerShot = 1, //heat generated per shot
-                    MaxHeat = 10000, //max heat before weapon enters cooldown (70% of max heat)
+                    HeatPerShot = 0, //heat generated per shot
+                    MaxHeat = 1000000, //max heat before weapon enters cooldown (70% of max heat)
                     Cooldown = .95f, //percent of max heat to be under to start firing again after overheat accepts .2-.95
-                    HeatSinkRate = 100, //amount of heat lost per second
+                    HeatSinkRate = 1000000, //amount of heat lost per second
                     DegradeRof = false, // progressively lower rate of fire after 80% heat threshold (80% of max heat)
-                    ShotsInBurst = 600,
+                    ShotsInBurst = 0,
                     DelayAfterBurst = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                 },
             },
@@ -90,8 +83,8 @@ namespace WeaponThread
             },
             Ammo = new AmmoDefinition
             {
-                BaseDamage = 3f,
-                Mass = 0f, // in kilograms
+                BaseDamage = 150f,
+                Mass = 1f, // in kilograms
                 Health = 0, // 0 = disabled, otherwise how much damage it can take from other trajectiles before dying.
                 BackKickForce = 0f,
                 Shape = Options(shape: Line, diameter: 0), //defines the collision shape of projectile, defaults to visual Line Length
@@ -122,7 +115,7 @@ namespace WeaponThread
                     TargetLossTime = 0, // 0 is disabled, Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     AccelPerSec = 0f,
                     DesiredSpeed = 400f,
-                    MaxTrajectory = 3000f,
+                    MaxTrajectory = 1500f,
                     SpeedVariance = Random(start: 0, end: 0), // subtracts value from DesiredSpeed
                     RangeVariance = Random(start: 0, end: 0), // subtracts value from MaxTrajectory
                     Smarts = new Smarts
@@ -163,10 +156,10 @@ namespace WeaponThread
                     },
                     Barrel1 = new Particle
                     {
-                        Name = "EnergyBlast", // Smoke_LargeGunShot
-                        Color = Color(red: 1, green: 5, blue: 1, alpha: 1),
+                        Name = "", // Smoke_LargeGunShot
+                        Color = Color(red: 1, green: 1, blue: 1, alpha: 1),
                         Offset = Vector(x: 0, y: -1, z: 0),
-                        Extras = Options(loop: true, restart: false, distance: 500, duration: 1, scale: 2f),
+                        Extras = Options(loop: true, restart: false, distance: 200, duration: 1, scale: 1f),
                     },
                     Barrel2 = new Particle
                     {
@@ -178,7 +171,7 @@ namespace WeaponThread
                 },
                 Line = new LineDefinition
                 {
-                    Tracer = Base(enable: true, length: 3f, width: 0.05f, color: Color(red: 0.9f, green: 1.9f, blue: 0.9f, alpha: 1)),
+                    Tracer = Base(enable: true, length: 3f, width: 0.05f, color: Color(red: 0.9f, green: 0.9f, blue: 0.9f, alpha: 1)),
                     TracerMaterial = "WeaponLaser", // WeaponLaser, ProjectileTrailLine, WarpBubble, etc..
                     ColorVariance = Random(start: 5f, end: 10f), // multiply the color by random values within range.
                     WidthVariance = Random(start: 0f, end: 0.045f), // adds random value to default width (negatives shrinks width)
@@ -189,18 +182,18 @@ namespace WeaponThread
             {
                 HardPoint = new AudioHardPointDefinition
                 {
-                    FiringSound = "", // WepShipGatlingShot
+                    FiringSound = "ArcWepShipGatlingShot", // WepShipGatlingShot
                     FiringSoundPerShot = true,
                     ReloadSound = "",
                     NoAmmoSound = "",
-                    HardPointRotationSound = "",
-                    BarrelRotationSound = "",
+                    HardPointRotationSound = "WepTurretGatlingRotate",
+                    BarrelRotationSound = "WepShipGatlingRotation",
                 },
 
                 Ammo = new AudioAmmoDefinition
                 {
                     TravelSound = "",
-                    HitSound = "",
+                    HitSound = "ArcImpMetalMetalCat0",
                 }, // Don't edit below this line
             },
         };
