8d0af2ac71b8d4c16e76acc28583c1b8fca73361
Bryon <30876973+nukeguard@users.noreply.github.com>
2019-12-09 19:06:01 -0500

    Update NtechObelisk_Animations.cs

--- Data/Scripts/WeaponThread/NtechObelisk_Animations.cs
+++ Data/Scripts/WeaponThread/NtechObelisk_Animations.cs
@@ -527,7 +527,7 @@ namespace WeaponThread
 				new PartAnimationSetDef()
                 {
                     SubpartId = Names("Crystal_Edge1"),
-                    BarrelId = "muzzle_barrel_01", //only used for firing, use "Any" for all muzzles
+                    BarrelId = "muzzle_barrel_02", //only used for firing, use "Any" for all muzzles
                     StartupFireDelay = 0,
                     AnimationDelays = Delays(FiringDelay : 0, ReloadingDelay: 0, OverheatedDelay: 0, TrackingDelay: 0, LockedDelay: 0, OnDelay: 0, OffDelay: 0, BurstReloadDelay: 0, OutOfAmmoDelay: 0, PreFireDelay: 0, StopFiringDelay: 0),//Delay before animation starts
                     Reverse = Events(), // (Firing, Overheated)
@@ -580,7 +580,7 @@ namespace WeaponThread
 				new PartAnimationSetDef()
                 {
                     SubpartId = Names("Crystal_Edge2"),
-                    BarrelId = "muzzle_barrel_01", //only used for firing, use "Any" for all muzzles
+                    BarrelId = "muzzle_barrel_03", //only used for firing, use "Any" for all muzzles
                     StartupFireDelay = 0,
                     AnimationDelays = Delays(FiringDelay : 0, ReloadingDelay: 0, OverheatedDelay: 0, TrackingDelay: 0, LockedDelay: 0, OnDelay: 0, OffDelay: 0, BurstReloadDelay: 0, OutOfAmmoDelay: 0, PreFireDelay: 0, StopFiringDelay: 0),//Delay before animation starts
                     Reverse = Events(), // (Firing, Overheated)
@@ -634,7 +634,7 @@ namespace WeaponThread
 				new PartAnimationSetDef()
                 {
                     SubpartId = Names("Crystal_Edge3"),
-                    BarrelId = "muzzle_barrel_01", //only used for firing, use "Any" for all muzzles
+                    BarrelId = "muzzle_barrel_04", //only used for firing, use "Any" for all muzzles
                     StartupFireDelay = 0,
                     AnimationDelays = Delays(FiringDelay : 0, ReloadingDelay: 0, OverheatedDelay: 0, TrackingDelay: 0, LockedDelay: 0, OnDelay: 0, OffDelay: 0, BurstReloadDelay: 0, OutOfAmmoDelay: 0, PreFireDelay: 0, StopFiringDelay: 0),//Delay before animation starts
                     Reverse = Events(), // (Firing, Overheated)
@@ -687,7 +687,7 @@ namespace WeaponThread
 				new PartAnimationSetDef()
                 {
                     SubpartId = Names("Crystal_Edge4"),
-                    BarrelId = "muzzle_barrel_01", //only used for firing, use "Any" for all muzzles
+                    BarrelId = "muzzle_barrel_05", //only used for firing, use "Any" for all muzzles
                     StartupFireDelay = 0,
                     AnimationDelays = Delays(FiringDelay : 0, ReloadingDelay: 0, OverheatedDelay: 0, TrackingDelay: 0, LockedDelay: 0, OnDelay: 0, OffDelay: 0, BurstReloadDelay: 0, OutOfAmmoDelay: 0, PreFireDelay: 0, StopFiringDelay: 0),//Delay before animation starts
                     Reverse = Events(), // (Firing, Overheated)
