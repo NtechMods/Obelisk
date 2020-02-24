45eaa0ffba7bd8d87b767e937beae6cb9ea8f31d
Bryon <30876973+nukeguard@users.noreply.github.com>
2020-02-24 18:50:06 -0500

    Block Variants

--- Data/Scripts/WeaponThread/NtechObelisk3.cs
+++ Data/Scripts/WeaponThread/NtechObelisk3.cs
@@ -58,7 +58,7 @@ namespace WeaponThread
             },
             Targeting = new TargetingDefinition
             {
-                Threats = Valid(Grids),
+                Threats = Valid(Grids, Meteors, Projectiles, Characters),
                 SubSystems = Priority(Power, Thrust, Offense, Utility, Production, Any), //define block type targeting order
                 ClosestFirst = true, // tries to pick closest targets first (blocks on grids, projectiles, etc...).
                 MinimumDiameter = 0, // 0 = unlimited, Minimum radius of threat to engage.
